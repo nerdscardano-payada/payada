0f8e3f37d0aed9e1bb557595b7d2dccea2ddd194
Bruno <nerdscardano@gmail.com>
2026-03-03 15:31:50 +0000

    File changes

--- functions/recordWalletPayment.ts
+++ functions/recordWalletPayment.ts
@@ -1,4 +1,5 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
+// Force latest SDK - fixes app.base44.com DNS issue
 
 const BLOCKFROST_API_KEY = Deno.env.get("BLOCKFROST_API_KEY");
 const BLOCKFROST_URL = "https://cardano-mainnet.blockfrost.io/api/v0";
