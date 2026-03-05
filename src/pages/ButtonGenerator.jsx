86e1315f2be68b6a1accc985e6dd228eaf27c2e7
Bruno <nerdscardano@gmail.com>
2026-03-05 08:51:07 +0000

    File changes

--- src/pages/ButtonGenerator.jsx
+++ src/pages/ButtonGenerator.jsx
@@ -93,7 +93,7 @@ const ICONS = [
     ),
   },
   {
-    label: "Bliksem / Zap",
+    label: "Zap",
     key: "zap",
     svg: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" style="display:inline;vertical-align:middle;margin-right:7px;margin-bottom:1px"><path d="M13 2 3 14h9l-1 8 10-12h-9l1-8z"/></svg>`,
     jsx: (
