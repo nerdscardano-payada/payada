81674d1f04b9a73e75bdff33e9902fcb1d0494d0
Bruno <nerdscardano@gmail.com>
2026-03-05 08:50:27 +0000

    File changes

--- src/pages/ButtonGenerator.jsx
+++ src/pages/ButtonGenerator.jsx
@@ -73,7 +73,7 @@ const ICONS = [
     ),
   },
   {
-    label: "Pijl / Arrow",
+    label: "Arrow",
     key: "arrow",
     svg: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" style="display:inline;vertical-align:middle;margin-right:7px;margin-bottom:1px"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>`,
     jsx: (
