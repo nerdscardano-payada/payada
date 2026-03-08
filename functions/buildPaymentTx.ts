2516211617742bd1fafc766bd7fb86648de726e2
Bruno <nerdscardano@gmail.com>
2026-03-08 22:15:20 +0000

    File changes

--- functions/buildPaymentTx.ts
+++ functions/buildPaymentTx.ts
@@ -307,12 +307,12 @@ Deno.serve(async (req) => {
       const outputsData = [];
 
       // Merchant CNT output
-      outputsData.push({ addrBytes: merchantAddrBytes, lovelace: MIN_CNT_OUTPUT_LOVELACE, assets: merchantTokens });
+      outputsData.push({ addrBytes: merchantAddrBytes, lovelace: actualMerchantAda, assets: merchantTokens });
 
       // Fee CNT output
       if (cntFee > 0n && PAYADA_FEE_WALLET) {
         const feeAddrBytes = getAddrBytes(PAYADA_FEE_WALLET);
-        outputsData.push({ addrBytes: feeAddrBytes, lovelace: MIN_CNT_OUTPUT_LOVELACE, assets: feeTokens });
+        outputsData.push({ addrBytes: feeAddrBytes, lovelace: actualFeeAda, assets: feeTokens });
       }
 
       // Change output (ADA + remaining CNT if any)
